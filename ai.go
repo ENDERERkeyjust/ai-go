0fda17816e990bfbeef748dd4599eec59f263ec5
ENDERERkeyjust <sadaddsidas@gmail.com>
2024-08-31 12:15:52 +0300

    Add files via upload

---
+++ ai.go
@@ -0,0 +1,98 @@
+package ai
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const (
+	API = "https://www.blackbox.ai/api/chat"
+)
+
+type Message struct {
+	Author  string
+	Content string
+}
+
+type AgentMode struct {
+	Id   string `json:"id"`
+	Mode bool   `json:"mode"`
+}
+
+type RequestBody struct {
+	CodeModelMode     bool             `json:"codeModelMode"`
+	ClickedAnswer3    bool             `json:"clickedAnswer3"`
+	AgentMode         AgentMode        `json:"agentMode"`
+	TrendingAgentMode AgentMode        `json:"trendingAgentMode"`
+	Messages          []RequestMessage `json:"messages"`
+	Temperature       float32          `json:"temperature"`
+}
+
+type RequestMessage struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type ChatBot struct {
+	History []Message
+	Client  *http.Client
+}
+
+func (bot *ChatBot) Send(body RequestBody, Username string) string {
+
+	byteData, err := json.Marshal(body)
+	if err != nil {
+		fmt.Println(err)
+		return "```\n" + err.Error() + "\n```"
+	}
+
+	bot.History = append(bot.History, Message{
+		Author:  Username,
+		Content: body.Messages[0].Content,
+	})
+
+	req, err := http.NewRequest("POST", API, bytes.NewBuffer(byteData))
+	if err != nil {
+		fmt.Println(err)
+		return "```\n" + err.Error() + "\n```"
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := bot.Client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return "```\n" + err.Error() + "\n```"
+	}
+
+	defer resp.Body.Close()
+
+	bodyByte, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return "```\n" + err.Error() + "\n```"
+	}
+
+	bot.History = append(bot.History, Message{
+		Author:  "You",
+		Content: string(bodyByte),
+	})
+
+	return string(bodyByte)
+}
+
+func (bot *ChatBot) GetHistory() string {
+	var history string
+	for i := len(bot.History) - 1; i > 0; i-- {
+		dif := (len(bot.History) - 1) - i
+		if dif > 25 {
+			break
+		}
+		msg := bot.History[i]
+		history += msg.Author + " messaged \"" + msg.Content + "\";\n"
+	}
+	return history
+}
